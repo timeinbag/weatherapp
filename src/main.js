1f70d500b2b68529a91981b5576364534ddc9fe5
Arch <timeinbag@gmail.com>
2021-01-16 23:07:03 +0200

    add mvc

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
-class App {
-    
-}
+// import View from './view';
+// import Model from './model';
+// import Controller from './controller';
+// document.addEventListener('DOMContentLoaded', () => {
+// let view = new View();
+// let model = new Model(view);
+// let controller = new Controller(model);
+// controller._initApp();
+// })
